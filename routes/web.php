236322a7d8bcfd30b082d0c8f66b486681f5b679
Lester Fong <lester.fong@switchconnect.com.au>
2023-10-10 20:16:58 +0800

    updated the route in web.php for the url('oauth/token') api request to work

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::any('/', function () {
+Route::get('/{any}', function () {
     return view('welcome');
-});
+})->where('any', '.*');
+
+
+date_default_timezone_set('Asia/Manila');
