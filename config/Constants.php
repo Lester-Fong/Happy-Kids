f3e3f1f54a7d1abad6586155ba6642ca678240b5
Lester Fong <lester.fong@switchconnect.com.au>
2023-10-10 20:13:56 +0800

    created constants variables

---
+++ config/Constants.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'ERROR_MESSAGES' => [
+        'ACCOUNT_INACTIVE' => 'Your account is inactive. Please contact the administrator.',
+        'SUCCESS_LOGIN' => 'You successfully log in',
+        'ERROR_LOGIN' => 'Invalid email or password',
+    ]
+];
