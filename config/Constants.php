84547f50d4597137851d8b63290706fad46b2f80
Lester Fong <lester.fong@switchconnect.com.au>
2023-10-24 19:28:20 +0800

    config files updates

--- config/Constants.php
+++ config/Constants.php
@@ -1,9 +1,16 @@
 <?php
 
 return [
-    'ERROR_MESSAGES' => [
+    'ERROR_MESSAGE' => [
         'ACCOUNT_INACTIVE' => 'Your account is inactive. Please contact the administrator.',
         'SUCCESS_LOGIN' => 'You successfully log in',
         'ERROR_LOGIN' => 'Invalid email or password',
+        'FORGOT_PASSWORD_SUCCESSFUL' => 'Please check your email for the verification code.',
+        'EMAIL_ADDRESS_NOT_FOUND' => 'Email address not found.',
+        'OTP_EXPIRED' => 'Your OTP is expired.',
+        'OTP_VALID' => 'Your OTP is valid.',
+        'OTP_INVALID' => 'Your OTP is invalid.',
+        'INVALID_SECURITY_CODE' => 'Invalid security code.',
+        'RESET_PASSWORD_SUCCESSFUL' => 'Your password has been reset successfully.',
     ]
 ];
