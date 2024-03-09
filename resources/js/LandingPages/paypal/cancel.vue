0ba66927ef11c0fd8de1e9799568aa508935fd4c
Karl Rito <karllouiserito08@gmail.com>
2024-03-09 18:48:39 +0800

    Add cancel.vue component for PayPal transaction cancellation

---
+++ resources/js/LandingPages/paypal/cancel.vue
@@ -0,0 +1,27 @@
+<template>
+    <!-- cancel -->
+    <div class="container mt-5 py-5">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="alert alert-danger" role="alert">
+                    <h4 class="alert-heading">Transaction Cancelled!</h4>
+                    <p>Your transaction has been cancelled. Please try again.</p>
+                    <hr />
+                    <p class="mb-0">
+                        <a href="/" class="btn btn-primary">Go Back</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            token: this.$route.query.token,
+        };
+    },
+};
+</script>
