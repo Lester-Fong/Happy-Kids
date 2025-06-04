2f5308c6fb4ef89800024e1c8dd40926eda7aef3
Lester Fong <lesterfonggi123@gmail.com>
2025-06-04 18:48:30 +0800

    path updates

--- resources/views/email.blade.php
+++ resources/views/email.blade.php
@@ -1,46 +1,61 @@
 <div style="font-family: 'Poppins', sans-serif !important;">
     <div style="width:100%; display: flex; justify-content: center;">
-        <div width="600" border="0" cellpadding="0" cellspacing="0" role="presentation" bgcolor="#ffffff" style="background-color:#ffffff;width:600px"">
+        <div width="600" border="0" cellpadding="0" cellspacing="0" role="presentation" bgcolor="#ffffff"
+            style="background-color:#ffffff;width:600px"">
             <div style=" background-color: rgba(0,0,0, 0.03); width: 100%; min-height: 420px; margin-top: 13px;">
-            <div style="background: #fff; width: 100%; min-height: 420px; position: relative; overflow: hidden; display: flex; justify-content: center;">
+            <div
+                style="background: #fff; width: 100%; min-height: 420px; position: relative; overflow: hidden; display: flex; justify-content: center;">
                 <div style="padding: 20px 20px 0;">
-                    {{-- <img src="{{asset('/public/images/happy_kids_logo.jpg')}}" style="display: block; margin: auto; margin-bottom: 16px;"> --}}
+                    {{-- <img src="{{asset('/images/happy_kids_logo.jpg')}}"
+                        style="display: block; margin: auto; margin-bottom: 16px;"> --}}
                     <p style="font-size: 14px;">
-                        <table style="margin: auto; padding:0 1rem;" border="0" cellpadding="0" cellspacing="0" width="500">
-                            <tr>
-                                <td>
-                                    {!!$content!!}
-                                </td>
-                            </tr>
+                    <table style="margin: auto; padding:0 1rem;" border="0" cellpadding="0" cellspacing="0" width="500">
+                        <tr>
+                            <td>
+                                {!!$content!!}
+                            </td>
+                        </tr>
 
 
 
-                            {{-- FOOTER --}}
-                            <tr>
-                                <td valign="top">
-                                    <table style="margin-top: 16px;" align="center" width="600" border="0" cellpadding="0" cellspacing="0" role="presentation" bgcolor="#800f2f" style="background-color:#800f2f;width:600px">
-                                        <tbody>
-                                            <tr>
-                                                <td width="22" style="width:22px">&nbsp;</td>
-                                                <td align="center" style="color:#ffffff;font-size:11px;line-height:14px;padding-top:30px;text-align:center">
-                                                    <strong>
-                                                        <a href="http://happy_kids.local/admin/" style="color:#ffffff;font-weight:bold;text-decoration:none" target="_blank">Manage Your Account</a> |
-                                                        <a href="mailto:support@trimexcolleges.edu.ph" style="color:#ffffff;font-weight:bold;text-decoration:none" target="_blank">Support: support@trimexcolleges.edu.ph</a>
-                                                    </strong>
-                                                </td>
-                                                <td width="22" style="width:22px">&nbsp;</td>
-                                            </tr>
-                                            <tr>
-                                                <td width="22" style="width:22px">&nbsp;</td>
-                                                <td style="color:#e9cbcbf4;font-size:11px;line-height:14px;padding-top:30px;padding-bottom:30px">Happy Kids and the Happy Kids logo are either registered trademarks or trademarks of Happy Kids in the Philippines. All other trademarks are the property of their respective owners.<br><br> Office: Trojan Building, Plaza Limaco, Poblacion, Biñan City, Laguna, Philippines
-                                                </td>
-                                                <td width="22" style="width:22px">&nbsp;</td>
-                                            </tr>
-                                        </tbody>
-                                    </table>
-                                </td>
-                            </tr>
-                        </table>
+                        {{-- FOOTER --}}
+                        <tr>
+                            <td valign="top">
+                                <table style="margin-top: 16px;" align="center" width="600" border="0" cellpadding="0"
+                                    cellspacing="0" role="presentation" bgcolor="#800f2f"
+                                    style="background-color:#800f2f;width:600px">
+                                    <tbody>
+                                        <tr>
+                                            <td width="22" style="width:22px">&nbsp;</td>
+                                            <td align="center"
+                                                style="color:#ffffff;font-size:11px;line-height:14px;padding-top:30px;text-align:center">
+                                                <strong>
+                                                    <a href="http://happy_kids.local/admin/"
+                                                        style="color:#ffffff;font-weight:bold;text-decoration:none"
+                                                        target="_blank">Manage Your Account</a> |
+                                                    <a href="mailto:support@trimexcolleges.edu.ph"
+                                                        style="color:#ffffff;font-weight:bold;text-decoration:none"
+                                                        target="_blank">Support: support@trimexcolleges.edu.ph</a>
+                                                </strong>
+                                            </td>
+                                            <td width="22" style="width:22px">&nbsp;</td>
+                                        </tr>
+                                        <tr>
+                                            <td width="22" style="width:22px">&nbsp;</td>
+                                            <td
+                                                style="color:#e9cbcbf4;font-size:11px;line-height:14px;padding-top:30px;padding-bottom:30px">
+                                                Happy Kids and the Happy Kids logo are either registered trademarks or
+                                                trademarks of Happy Kids in the Philippines. All other trademarks are
+                                                the property of their respective owners.<br><br> Office: Trojan
+                                                Building, Plaza Limaco, Poblacion, Biñan City, Laguna, Philippines
+                                            </td>
+                                            <td width="22" style="width:22px">&nbsp;</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </td>
+                        </tr>
+                    </table>
                     </p>
                 </div>
             </div>
